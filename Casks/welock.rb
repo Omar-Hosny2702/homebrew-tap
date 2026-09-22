004842b38f250fd35531407b2b9ee5c87fc211f2
Omar-Hosny2702 <omarhosny2702@gmail.com>
2026-09-22 22:23:04 +0300

    fix: match WeLock V1.2.3 release tag

--- Casks/welock.rb
+++ Casks/welock.rb
@@ -2,7 +2,7 @@ cask "welock" do
   version "1.2.3"
   sha256 "f3b8cc1459efb1fce3e342b2b558ec7469ac560c77fafa18566321bed61f100c"
 
-  url "https://github.com/Omar-Hosny2702/WeLock/releases/download/1.2.3/WeLock-v1.2.3.zip"
+  url "https://github.com/Omar-Hosny2702/WeLock/releases/download/V1.2.3/WeLock-v1.2.3.zip"
   name "WeLock"
   desc "Native macOS app-lock utility using system authentication"
   homepage "https://github.com/Omar-Hosny2702/WeLock"
